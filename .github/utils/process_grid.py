4be3fbab724f0df3e9a84825c3a44c18c9810149
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-20 18:16:22 +0300

    fix typo

--- .github/utils/process_grid.py
+++ .github/utils/process_grid.py
@@ -4,7 +4,7 @@ import json
 def main():
     grid = yaml.load(open("grid.yaml"))
     grid_json = json.dumps(grid)
-    print)f"::set-output name=matrix::{grid_json}")
+    print(f"::set-output name=matrix::{grid_json}")
 
 if __name__ == "__main__":
     main()
