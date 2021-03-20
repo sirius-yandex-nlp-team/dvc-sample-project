efc55d8460a96937d68c191cd40af301c5c0571e
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-03-20 18:29:19 +0300

    flatten matrix

--- .github/utils/process_grid.py
+++ .github/utils/process_grid.py
@@ -1,8 +1,15 @@
 import yaml
 import json
 
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+
 def main():
     grid = yaml.load(open("grid.yaml"))
+    grid = product_dict(**grid)
     grid_json = json.dumps(grid)
     print(f"::set-output name=matrix::{grid_json}")
 
