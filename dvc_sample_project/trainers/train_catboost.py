6f42565588820062cc47f8981276efe742e17570
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-04-05 16:55:31 +0300

    add dvc=True flag to logger

--- dvc_sample_project/trainers/train_catboost.py
+++ dvc_sample_project/trainers/train_catboost.py
@@ -17,8 +17,8 @@ def train_catboost(train_df, test_df):
     with kts.parse_stdout(kts.patterns.catboost, kts.LoggerCallback(logger=logger)):
         model = CatBoostClassifier(n_estimators=params.catboost.n_estimators)
         model.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_test, y_test)])
-    logger.log_metric("train_accuracy", accuracy_score(y_train, model.predict(x_train)))
-    logger.log_metric("test_accuracy", accuracy_score(y_test, model.predict(x_test)))
+    logger.log_metric("train_accuracy", accuracy_score(y_train, model.predict(x_train)), dvc=True)
+    logger.log_metric("test_accuracy", accuracy_score(y_test, model.predict(x_test)), dvc=True)
     return model
 
 if __name__ == "__main__":
